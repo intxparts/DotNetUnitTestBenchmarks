79b7daa0dc3ed0067daa21a2fac4e86dae11d720
intxparts <4543613+intxparts@users.noreply.github.com>
2021-01-10 16:23:02 -0500

    update FastTest dependency. Simplify test project.

--- FastTests/Program.cs
+++ FastTests/Program.cs
@@ -7,23 +7,11 @@ namespace FastTests
     {
         static void Main(string[] args)
         {
-            var timeMilliseconds = Test.Time(() => {
+            TestConsole.RunTests(() => {
                 Test.Run("hello", () => {
                     Assert.Pass();
                 });
             });
-
-            var failedTestData = Test.FailedTestData;
-            foreach (var f in failedTestData)
-            {
-                Console.WriteLine($"Test '{f.Key}' failed: {f.Value.Message}\n{f.Value.StackTrace}");
-            }
-
-            Console.WriteLine($"Test Count: {Test.TestCount}");
-            Console.WriteLine($"Tests Passed: {Test.PassedTests}");
-            Console.WriteLine($"Tests Failed: {(Test.TestCount - Test.PassedTests)}");
-            Console.WriteLine($"elapsed milliseconds: {timeMilliseconds}");
-            Console.WriteLine($"elapsed seconds: {(timeMilliseconds / 1000.0)}");
         }
     }
 }
